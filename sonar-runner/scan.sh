fb5db16a2ab93ccef6ff11120a239fe6e372b95c
Angelika Ehlers <angelika.ehlers@gov.bc.ca>
2018-01-23 19:13:10 -0800

    Update scan.sh

--- sonar-runner/scan.sh
+++ sonar-runner/scan.sh
@@ -1,6 +1,6 @@
 cd ..
 cd Server
-mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=http://sonarqube-agehlers-sandbox.pathfinder.gov.bc.ca /d:sonar.host.port=80 /k:HETSSERVER
+mono /usr/lib/sonar-scanner/SonarQube.Scanner.MSBuild.exe begin /d:sonar.host.url=http://sonarqube-agehlers-sandbox.pathfinder.gov.bc.ca /d:sonar.host.port=80 /k:SANDBOXTEST
 dotnet restore
 dotnet build
 dotnet test
